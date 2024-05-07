64113ededddbc79cf8c7f5a255fb0249217b4ca6
Thanushree0527 <167883852+Thanushree0527@users.noreply.github.com>
2024-05-07 15:44:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,6 @@ app = Flask(__name__)
 app.config = ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db =SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100),unique=True)
-    password = db.Column(db.String(100))
-
-    def __iniit__(self,email,password):
-        self.email = email
-        self.password = bcrypt.hashpw(password.encode(utf-8)),
 
 
 
